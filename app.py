e3b1ca327b330845f3fc0d8f5aeab15ab27542d7
Siri Sure <115160129+siris11@users.noreply.github.com>
2024-09-18 22:52:37 +0000

    revert file added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import pickle
+import pickle,lzma
 import streamlit as st
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
@@ -87,41 +87,8 @@ with st.container():
         st.markdown(heading_animation, unsafe_allow_html=True)
 
 
-music = pickle.load(open('data.pkl','rb'))
-similarity = pickle.load(open('similarity.pkl','rb'))
-
-song_list = music['song'].values
-selected_song = st.selectbox("Pick your favourite songs🎵",
-                             song_list)
 
 
-if st.button('Confirm Selection'):
-    recommended_music_names,recommended_music_posters = recommend(selected_song)
-    col1,col2,col3,col4,col5,col6,col7,col8 = st.columns(8)
-    with col1:
-        st.text(recommended_music_names[0])
-        st.image(recommended_music_posters[0])
-    with col2:
-        st.text(recommended_music_names[1])
-        st.image(recommended_music_posters[1])
-    with col3:
-        st.text(recommended_music_names[2])
-        st.image(recommended_music_posters[2])
-    with col4:
-        st.text(recommended_music_names[3])
-        st.image(recommended_music_posters[3])
-    with col1:
-        st.text(recommended_music_names[4])
-        st.image(recommended_music_posters[4])
-    with col2:
-        st.text(recommended_music_names[5])
-        st.image(recommended_music_posters[5])
-    with col3:
-        st.text(recommended_music_names[6])
-        st.image(recommended_music_posters[6])
-    with col4:
-        st.text(recommended_music_names[7])
-        st.image(recommended_music_posters[7])
 
     
 # ---------------------------------------------------------------------------------------------- #
